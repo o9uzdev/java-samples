235967e4575ea5cf1c32556f678fdaa5a062fa03
o9uzdev <o9uzdev@gmail.com>
2021-04-10 21:57:08 +0300

    Added Variable Arguman Java Example

---
+++ general/VariableArgumanTest.java
@@ -0,0 +1,17 @@
+// Variable Arguman Test Java Example
+
+public class VariableArgumanTest {
+    public static int sum(int... nums){
+        int sum = 0;
+        for (int num : nums){
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum());
+        System.out.println(sum(1));
+        System.out.println(sum(1,3,4,5));
+    }
+}
