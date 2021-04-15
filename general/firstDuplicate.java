1ca6b7b22b4a61c1f16393301d0f925fe0ab07a8
o9uzdev <o9uzdev@gmail.com>
2021-04-15 11:01:45 +0300

    added firstDuplicate example

---
+++ general/firstDuplicate.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class firstDuplicate {
+    public static void main(String[] args) {
+        int[] test1 = {2, 1, 3, 5, 3, 2};
+        int[] test2 = {2, 1, 3, 5, 3, 2};
+
+        System.out.println(firstDuplicate(test1));
+        System.out.println(firstDuplicate(test2));
+    }
+
+    static int firstDuplicate(int[] a) {
+        Set<Integer> set = new HashSet<>();
+        for(int i:a){
+            if(!set.add(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
