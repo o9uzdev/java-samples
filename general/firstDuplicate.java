0f14306d9b0683e893d9366d619db6b61c4fc185
o9uzdev <o9uzdev@gmail.com>
2021-04-15 11:03:46 +0300

    updated firstDuplicate example

--- general/firstDuplicate.java
+++ general/firstDuplicate.java
@@ -4,7 +4,7 @@ import java.util.Set;
 public class firstDuplicate {
     public static void main(String[] args) {
         int[] test1 = {2, 1, 3, 5, 3, 2};
-        int[] test2 = {2, 1, 3, 5, 3, 2};
+        int[] test2 = {1, 1, 3, 5, 3, 2};
 
         System.out.println(firstDuplicate(test1));
         System.out.println(firstDuplicate(test2));
